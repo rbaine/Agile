530ee083d7fab8d91542f333c3b02b963677e19b
Rod <rbaine@symbience.com>
2015-03-28 15:16:03 -0700

    add sortable.js and enable drag & drop between connected list and get new sort order in AngularJS controller

--- agilejs/js/app.js
+++ agilejs/js/app.js
@@ -1,7 +1,24 @@
-// var app = angular.module('app', ['snap']);
-var app = angular.module('app', []);
-
+var app = angular.module('app', ['ui.sortable']);
 app.controller('MainCtrl', function($scope) {
-  $scope.name = 'World';
-  $scope.ver = angular.version.full;
+	$scope.name = 'World';
+	$scope.ver = angular.version.full;
+	$scope.itemsA = ['#100', '#101', '#102', '#103', '#104', '#105', '#106', '#107', '#108', '#109', '#110', '#111', '#112', '#113'];
+	$scope.itemsB = ['#000'];
+
+	$scope.listStories = function () {
+		for (var i = 0; i < $scope.itemsA.length; i++) {
+			console.log($scope.itemsA[i]);
+		}
+
+		for (var i = 0; i < $scope.itemsB.length; i++) {
+			console.log($scope.itemsB[i]);
+		}
+	};
+
+	 $scope.sortableOptions = {
+    	placeholder: "grid",
+    	connectWith: [".source", ".target"]
+  	};
+
 });
+
